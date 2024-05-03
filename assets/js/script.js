1b76d2b8c30fba32aae992d9f7d4a0ab0d9b32e7
henriquefontes <70450246+henriquefontes@users.noreply.github.com>
2024-05-03 15:20:25 -0300

    auto resize table and ripple effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,52 @@ function changeModalVisibility(modal) {
     .classList.toggle("open");
 }
 
+function resizeTable() {
+  const table = document.querySelector(".table-wrapper");
+
+  table.style.height = window.innerHeight - table.offsetTop - 40 + "px";
+}
+
+function rippleEffect(button, { layerX, layerY }) {
+  const $ripple = document.createElement("span");
+
+  $ripple.classList.add("ripple");
+  $ripple.style.top = layerY - 10 + "px";
+  $ripple.style.left = layerX - 10 + "px";
+
+  button.appendChild($ripple);
+
+  const DIAMETER = button.clientWidth * 2;
+  const [R, G, B] = window
+    .getComputedStyle(button)
+    .getPropertyValue("background-color")
+    .match(/\d+/g);
+
+  $ripple.offsetHeight;
+
+  $ripple.style.width = DIAMETER + "px";
+  $ripple.style.height = DIAMETER + "px";
+  $ripple.style.top = layerY - DIAMETER / 2 + "px";
+  $ripple.style.left = layerX - DIAMETER / 2 + "px";
+  $ripple.style.opacity = "0";
+  $ripple.style.backgroundColor = `rgba(${R - 80}, ${G - 80}, ${B - 40}, 0.3)`;
+
+  setTimeout(() => {
+    $ripple.remove();
+  }, 500);
+}
+
+window.addEventListener("load", resizeTable);
+window.addEventListener("resize", resizeTable);
+
 document.querySelectorAll("[data-modal]").forEach((button) => {
   button.addEventListener("click", () => {
     changeModalVisibility(button.dataset.modal);
+  });
+});
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    rippleEffect(button, e);
   });
 });
